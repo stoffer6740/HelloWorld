cea909da232238f2330b1e68352eeae6709d8d1a
stoffer6740 <cbj@c-bjerregaard.dk>
2014-11-13 10:06:32 +0100

    Init

---
+++ app/app.js
@@ -0,0 +1,94 @@
+var app = angular.module('helloworld', []);
+
+
+app.controller('AppCtrl', function ($scope) {
+    $scope.title = 'App Controller';
+});
+
+
+app.controller('PageCtrl', function ($scope) {
+    $scope.title = 'Page Controller';
+
+    $scope.array = ['Hello','World','Lol'];
+
+    $scope.json = [
+                       {"id":1, "name":"test"},
+                       {"id":2, "name":"test2"},
+                       {"id":3, "name":"test3"}
+                  ];
+});
+
+app.controller('FormCtrl', function ($scope) {
+    $scope.title = "Form Controller";
+    $scope.added = [];
+
+    var i = 0;
+    $scope.addTo = function () {
+        $scope.added.push({"id":i, "txt":$scope.textToAdd});
+        $scope.textToAdd = "";
+        i++;
+    };
+
+    $scope.clear = function () {
+        $scope.added = [];
+        i = 0;
+    }
+});
+
+
+
+app.controller('BarcodeCtrl', function ($scope) {
+    $scope.title = 'Barcode scanner';
+
+    $scope.scanBarcode = function () {
+        cordova.plugins.barcodeScanner.scan(
+            function (result) {
+                alert("We got a barcode\n" +
+                "Result: " + result.text + "\n" +
+                "Format: " + result.format + "\n" +
+                "Cancelled: " + result.cancelled);
+            },
+            function (error) {
+                alert("Scanning failed: " + error);
+            }
+        );
+    };
+
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function onDeviceReady () {
+    alert("Hello world");
+}
+
